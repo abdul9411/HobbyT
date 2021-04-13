08a9aa4f231254cbb1b0ec748a59e3477befb979
ayaan09 <79858880+ayaan09@users.noreply.github.com>
2021-04-13 13:39:54 +0600

    Update post-templates-physics.js

--- Frontend/src/components/Feed/post-templates-physics.js
+++ Frontend/src/components/Feed/post-templates-physics.js
@@ -1,17 +1,1 @@
-const phy_templates= [
-    {
-        text: "Each and every action has same but opposite reaction",
-        username: "isaacnewton",
-        displayName: "Isaac Newton",
-        timestamp: "13 April",
-    },
-  
-    {
-        text: "Physics from creation to collapse",
-        username: "john",
-        displayName: "John Doe",
-        timestamp: "13 April"
-    },
-]
-
-export default phy_templates;
+//
