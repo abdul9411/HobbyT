a56884367394a5e1409d75855ee61a8a76c650af
ayaan09 <79858880+ayaan09@users.noreply.github.com>
2021-04-13 17:09:40 +0600

    Update Profile_data.js

--- Frontend/src/components/Feed/Profile/Profile_data.js
+++ Frontend/src/components/Feed/Profile/Profile_data.js
@@ -5,11 +5,18 @@ import React, { useState, useEffect } from 'react';
 import Homefeedheader from '../Templates/Homefeedheader'
 import { Button } from '@material-ui/core';
 import Posts from '../Templates/Posts'
-
+import Collapsible from 'react-collapsible'
+import TextareaAutosize from 'react-textarea-autosize'
 
 function Profile_data(props) {
     const user_id= props.username
     const [posts, setposts]= useState([])
+    const [bioval, changebioval] = useState("")
+    const [mybio, updatedbio]= useState(props.bio)
+    useEffect(()=>{
+      updatedbio(props.bio)
+      
+    },[props.bio])
     useEffect(() => {
       axios.defaults.headers.common['x-auth-token'] = props.token;
         axios.get(`http://localhost:3001/api/post/query`,{
@@ -23,6 +30,22 @@ function Profile_data(props) {
         })
       }, []);
 
+function textme(event){
+  changebioval(event.target.value)
+}
+     function updateBio(){
+        axios.defaults.headers.common['x-auth-token'] = props.token;
+        const bio = bioval
+          axios.patch(`http://localhost:3001/api/auth/user/bio`,{
+            user_id: user_id,
+            bio: bioval
+          })
+          .then((response)=>{
+            updatedbio(bio)
+            changebioval("")
+          }).catch((e)=>console.log(e))
+     }
+
     return (
         <div className="my_profile">
          <Homefeedheader
@@ -31,11 +54,16 @@ function Profile_data(props) {
            <div className="prof-desc">
            {/* need to adjust size */}
             <Avatar src={props.icon} className="prof-pic"/>
-            <div className="dp-name">{props.displayname}</div>
-            <div>@{props.username}</div>
-            <div>{props.bio}</div>
+            <div className="dp-name">{props.displayname} (@{props.username})</div>
+            <div className= "bio">{mybio}</div>
+            {console.log(props.bio)}
+            <Collapsible trigger= "Edit Profile">
             <Button>Add Profile Photo</Button>
-            <Button>Edit Bio</Button>
+            <TextareaAutosize maxlength='200' className= "bio-holder" placeholder="Tell us about your hobbies"
+            value = {bioval} onChange={textme}
+            />
+            <Button onClick={updateBio} >Update Bio</Button>
+            </Collapsible>
             </div>
             {posts.map(
             item=>(
