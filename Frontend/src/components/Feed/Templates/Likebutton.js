ce5f6be99e4b84a9964d03d9a12396084ef6dbc8
ayaan09 <79858880+ayaan09@users.noreply.github.com>
2021-04-13 13:36:24 +0600

    Update Likebutton.js

--- Frontend/src/components/Feed/Templates/Likebutton.js
+++ Frontend/src/components/Feed/Templates/Likebutton.js
@@ -1,20 +1,79 @@
 import React, {useEffect, useState} from 'react';
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
-import Button from '@material-ui/core'
 import './Likebutton.css'
+import axios from 'axios'
 
-// need to get likestate from likepost function
+// it takes two takes to dislike
 
-function Likebutton(props) {
+function Likebutton({post_id, 
+    user_id,
+    likeCount}) {
     const[likestate, changelikestate]= useState("unliked")
-    // useEffect
+    // const[posts, changePosts] = useState({})
+
+    const[nooflikes, changelikeno]= useState(0)
+
+
+    
+    useEffect(() => {
+        axios.get(`http://localhost:3001/api/likepost/query`,{
+        params: {
+          user_id: user_id,
+          post_id: post_id
+        }
+        })
+        .then((response)=>{
+          response.data.length!==0&&changelikestate("liked")
+        })
+      }, [post_id, likestate]);
+     
+      useEffect(() => {
+        axios.get(`http://localhost:3001/api/likepost/query`,{
+        params: {
+          post_id: post_id
+        }
+        })
+        .then((response)=>{
+            changelikeno(response.data.length)
+        })
+      }, [post_id]);
+
+const[likepostid, changelikepostid]= useState(0)
+
+      function updateLikes(){
+        axios.get(`http://localhost:3001/api/likepost/query`,{
+            params: {
+                user_id: user_id,
+                post_id: post_id
+              }
+              })
+              .then((response)=>{
+                changelikepostid(response.data[0].likepost_id)
+                console.log(response.data[0].likepost_id)
+            }).catch((error)=>console.log(error))
+          if (likestate==="liked"){
+                axios.delete(`http://localhost:3001/api/likepost`,{
+                data: {
+                    likepost_id:likepostid
+                    }
+                  }).then((response)=>{
+                      response.status==200&&changelikeno(nooflikes-1);
+                      changelikestate("unliked")
+                }, 
+                 ) .catch((error)=>console.log(error))
+          }else if (likestate==="unliked"){
+            axios.post(`http://localhost:3001/api/likepost`,{
+                    user_id: user_id,
+                    post_id: post_id
+                  }).then(changelikeno(nooflikes+1), changelikestate("liked")).catch((error)=>console.log(error))
+          }
+      }
+
     return (
         <div>
-        <button className="Likebutton" onClick={
-            ()=>likestate==="unliked"?changelikestate("liked"):changelikestate("unliked")
-        }>
+        <button className="Likebutton" onClick={updateLikes}>
             <ArrowUpwardIcon className="arrowIcon" fontSize= "small"  style={{color: likestate === "liked" ? "blue" : "black"}}/>
-            {props.likeCount}
+            {nooflikes}
         </button>
         </div>
     );
