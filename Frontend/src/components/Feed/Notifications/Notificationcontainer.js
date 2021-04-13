9a9cd877ffdb10b2d12ec88a272d5f2b7a4cffa7
ayaan09 <79858880+ayaan09@users.noreply.github.com>
2021-04-13 18:35:57 +0600

    Update Notificationcontainer.js

--- Frontend/src/components/Feed/Notifications/Notificationcontainer.js
+++ Frontend/src/components/Feed/Notifications/Notificationcontainer.js
@@ -8,6 +8,7 @@ function Notificationcontainer(props) {
         <Avatar className="avatar-notif" src ={props.src}/>
        
         <span className="text-notif"> {props.timestamp} : {props.displayname} has {props.action} your post</span>
+        <div>You posted: {props.content}</div>
       </div>
         
     );
