a8caa32f7f53fc83150304539080c3139564bff6
ayaan09 <79858880+ayaan09@users.noreply.github.com>
2021-04-13 13:20:45 +0600

    Update Community_tab.js

--- Frontend/src/components/Feed/Community/Community_tab.js
+++ Frontend/src/components/Feed/Community/Community_tab.js
@@ -5,24 +5,69 @@ import { Button } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import "./Community_tab.css"  
 
-//taking user props
-
 function Community_tab(props){
      const[joinstate, setstate] = useState("+Join");
-     const[hasjoined, changehasjoined]= useState(false);
+     const [community_id, changecommunityID] = useState(0);
+     const [community_user_id, changecommuserid]= useState(0)
      const userID= props.userID
+
      useEffect(() => {
-        axios.get(`${process.env.REACT_API_URL}/communityuser`,{
+      axios.defaults.headers.common['x-auth-token'] = props.token; 
+      axios.get(`http://localhost:3001/api/community/query`,{
+      params: {
+        name: props.name
+      }
+      })
+      .then((response)=>{
+        changecommunityID(response.data[0].community_id)
+      })
+      .catch((error)=>{
+        console.log(error)
+      })
+    }, []);
+
+     useEffect(() => {
+      axios.defaults.headers.common['x-auth-token'] = props.token;
+        axios.get(`http://localhost:3001/api/communityuser/query`,
+        {
         params: {
-          user_id:{userID}
+          user_id: userID,
+          community_id: community_id
         }
         })
         .then((response)=>{
-          hasjoined===false?setstate("+Join"):setstate("Joined")
-          // axios returns API response body in .data
+          console.log(response.data)
+          response.data.length!==0&&setstate("Joined")
+          response.data.length!==0&&changecommuserid(response.data[0].communityuser_id)
         })
-      }, [joinstate]);
-      
+        .catch((error)=>{
+          console.log(error)
+        })
+      }, [community_id]);
+
+      function Changejoin(){
+        if (joinstate==="Joined"){
+          setstate("+Join")
+          console.log(community_user_id)
+          axios.defaults.headers.common['x-auth-token'] = props.token;
+          axios.delete(`http://localhost:3001/api/communityuser`,{
+            data:{
+            communityuser_id: community_user_id}
+          }).catch((error)=>{
+            console.log(error)
+          })
+        }else if(joinstate==="+Join"){
+          setstate("Joined")
+          axios.defaults.headers.common['x-auth-token'] = props.token;
+          axios.post(`http://localhost:3001/api/communityuser`,{
+            user_id: userID,
+            community_id: community_id
+          }).catch((error)=>{
+            console.log(error)
+          })
+        }
+      }
+
     return (
         
         <div className="community-tab">
@@ -30,8 +75,7 @@ function Community_tab(props){
         <div className="cm-name">{props.name} Community</div>
         <div className="cm-description">{props.description}</div>
         </Link>
-        <div><Button onClick={
-            ()=>joinstate==="+Join"?setstate("Joined"):setstate("+Join")} className="join-btn">{joinstate}
+        <div><Button onClick={Changejoin} className="join-btn">{joinstate}
             </Button></div>
         </div>
     );
