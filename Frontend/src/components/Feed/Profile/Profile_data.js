d994e869bdc96a5d8d35d645e5c2c7e81dacb207
ayaan09 <79858880+ayaan09@users.noreply.github.com>
2021-04-14 05:40:31 +0600

    Update Profile_data.js

--- Frontend/src/components/Feed/Profile/Profile_data.js
+++ Frontend/src/components/Feed/Profile/Profile_data.js
@@ -1,6 +1,6 @@
 import './Profile_data.css'
 import axios from 'axios'
-import { Avatar } from '@material-ui/core';
+import Avatar from 'react-avatar';
 import React, { useState, useEffect } from 'react';
 import Homefeedheader from '../Templates/Homefeedheader'
 import { Button } from '@material-ui/core';
@@ -53,12 +53,11 @@ function textme(event){
            />
            <div className="prof-desc">
            {/* need to adjust size */}
-            <Avatar src={props.icon} className="prof-pic"/>
+            <Avatar color={Avatar.getRandomColor('sitebase', ['red', 'green', 'blue'])} round={true} name={props.displayname} className="prof-pic"/>
             <div className="dp-name">{props.displayname} (@{props.username})</div>
             <div className= "bio">{mybio}</div>
             {console.log(props.bio)}
             <Collapsible trigger= "Edit Profile">
-            <Button>Add Profile Photo</Button>
             <TextareaAutosize maxlength='200' className= "bio-holder" placeholder="Tell us about your hobbies"
             value = {bioval} onChange={textme}
             />
