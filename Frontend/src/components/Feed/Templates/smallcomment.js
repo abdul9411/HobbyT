dce56309d8fefdca4fd7ab3d498c6652bd88814b
ayaan09 <79858880+ayaan09@users.noreply.github.com>
2021-04-13 15:17:20 +0600

    Update smallcomment.js

--- Frontend/src/components/Feed/Templates/smallcomment.js
+++ Frontend/src/components/Feed/Templates/smallcomment.js
@@ -1,21 +1,37 @@
 import { Avatar } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import './smallcomment.css'
 import Collapsible from 'react-collapsible'
+import axios from 'axios';
 function Smallcomment({
     comment,
     userID,
     myID,
-    avatar
+    avatar,
+    commentID
 }) {
+
+    const[commentexists, commentdelete] = useState(true)
+    function deletecomment(){
+        console.log("entet")
+        console.log(commentID)
+        axios.delete(`http://localhost:3001/api/comment`, {
+            data: {
+                comment_id: commentID
+            }
+        }).then((response)=>{
+            commentdelete(false)
+        }).catch((error)=>console.log(error))
+    }
     return (
         <div>
-       
+       {commentexists===true&&
         <div className="a-comment">
         <Avatar/>@{userID} says: {comment}
-           <Collapsible trigger=" ••• " >{myID===userID&&<button className="btn btn-danger btn-sm">delete comment
+           <Collapsible trigger=" ••• " >{myID===userID&&<button onClick={deletecomment} className="btn btn-danger btn-sm">delete comment
            </button>}{myID!==userID&&<button className="btn btn-dark btn-sm">Report comment</button>}</Collapsible>
            </div>
+        }
         </div>
     );
 }
