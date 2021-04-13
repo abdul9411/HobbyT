3b03a7fec49f20ebdc6a10c29eb97f927ceb62fd
ayaan09 <79858880+ayaan09@users.noreply.github.com>
2021-04-13 13:47:29 +0600

    Update communityuser.js

--- 3100projectBackend/routes/api/communityuser.js
+++ 3100projectBackend/routes/api/communityuser.js
@@ -20,6 +20,36 @@ const auth = require ('../../middleware/auth');
   });
 
   /**
+   * @route   GET api/community/query
+   * @desc    show a list of community info
+   */
+
+     router.get("/query",auth, async (req, res)=> {
+        try {
+          if(!req.query.name && req.query.community_id){
+            const results = await Community.find({'community_id': req.query.community_id});
+            if(!results) throw Error('No community exist');
+            res.status(200).json(results);
+
+          }else if (req.query.name && !req.query.community_id) {
+            const results = await Community.find({"name" : req.query.name});
+            if(!results) throw Error('No community exist');
+            res.status(200).json(results);
+
+          }else if (req.query.name && req.query.community_id) {
+            const results = await Community.find({"name" : req.query.name ,"community_id": req.query.community_id});
+            if(!results) throw Error('No community exist');
+            res.status(200).json(results);
+          }
+
+        }
+        catch (e) {
+          res.status(400).json({ msg: e.message });
+        }
+      });
+
+
+  /**
    * @route   POST api/communityuser
    * @desc    perform communityuser creation
    */
