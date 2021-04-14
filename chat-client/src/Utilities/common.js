902fe25c645c3b93c3eae30b893a52e3f20e8f7a
aomnafea0 <49070400+abdul9411@users.noreply.github.com>
2021-04-14 16:22:01 +0800

    Delete chat-client directory

--- chat-client/src/Utilities/common.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line
-export default {
-  getInitialsFromName: (name) => {
-    const letters = String(name)
-      .split(" ")
-      .map((i) => i.charAt(0));
-    return letters.join("");
-  },
-};
