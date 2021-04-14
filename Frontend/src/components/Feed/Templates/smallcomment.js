304ab03e0d6e8d5c39aa9c7c115062e9a226571f
ayaan09 <79858880+ayaan09@users.noreply.github.com>
2021-04-14 18:02:35 +0600

    Update smallcomment.js

--- Frontend/src/components/Feed/Templates/smallcomment.js
+++ Frontend/src/components/Feed/Templates/smallcomment.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import './smallcomment.css'
 import Collapsible from 'react-collapsible'
 import axios from 'axios';
+import { confirmAlert } from 'react-confirm-alert'; 
 function Smallcomment({
     comment,
     userID,
@@ -21,13 +22,26 @@ function Smallcomment({
             commentdelete(false)
         }).catch((error)=>console.log(error))
     }
+
+    function reportcomm(){
+        confirmAlert({
+            title: 'We value your opinions!',
+            message: `you have flagged comment#${commentID} as inappropriate. Please contact system admin with the comment ID and your complain`,
+            buttons: [
+              {
+                label: 'Ok',
+                onClick: () => null
+              }
+            ]
+          });
+    }
     return (
         <div>
        {commentexists===true&&
         <div className="a-comment">
         <Avatar/>@{userID} says: {comment}
            <Collapsible trigger=" ••• " >{myID===userID&&<button onClick={deletecomment} className="btn btn-danger btn-sm">Delete comment
-           </button>}{myID!==userID&&<button className="btn btn-dark btn-sm">Report comment</button>}</Collapsible>
+           </button>}{myID!==userID&&<button className="btn btn-dark btn-sm" onClick={reportcomm}>Report comment</button>}</Collapsible>
            </div>
         }
         </div>
