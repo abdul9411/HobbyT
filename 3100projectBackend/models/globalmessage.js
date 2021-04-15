4b967c5b91c778839bf1e7152e19bb0902d1442c
ayaan09 <79858880+ayaan09@users.noreply.github.com>
2021-04-16 03:53:37 +0600

    Update globalmessage.js

--- 3100projectBackend/models/globalmessage.js
+++ 3100projectBackend/models/globalmessage.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const GlobalMessageSchema = new Schema({
-	from: [{ type: Schema.Types.ObjectId, ref: 'users' }],
+	from: { 
+    type: String,
+    required: true
+   },
 	body: {
     type: String,
     required: true
