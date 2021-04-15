181431773574f5c85310308ae189ca3fd2bd41c2
fabdullah230 <80105828+fabdullah230@users.noreply.github.com>
2021-04-16 01:49:45 +0800

    updating carousel buttons and pages

--- Frontend/src/components/Feed/Templates/carousel.js
+++ Frontend/src/components/Feed/Templates/carousel.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'  
 import Carousel from 'react-bootstrap/Carousel'  
+import "./carousel.css"
 function BootstrapCarousel(props){  
                return (  
 
@@ -15,10 +16,10 @@ function BootstrapCarousel(props){
 
                          className="d-block w-100"  
 
-                        src={'https://www.fg-a.com/wallpapers/2021-black-random-stones.jpg'}  />  
+                        src={'https://cdn3.iconfinder.com/data/icons/google-suits-1/32/30_adsense_money_earn_advertise_ads_google-512.png'}  />  
 
                            <Carousel.Caption>  
-                           <h1>HobbyT is looking for advertisements</h1>
+                           <h1 className="ad">HobbyT is looking for advertisements</h1>
                                  </Carousel.Caption>  
 
                                  </Carousel.Item  >  
@@ -29,10 +30,10 @@ function BootstrapCarousel(props){
 
                                    className="d-block w-100"  
 
-                                    src={"https://www.fg-a.com/wallpapers/2021-black-random-stones.jpg"}    />  
+                                    src={"https://lh3.googleusercontent.com/proxy/3W3yfWR_zguwbUx8e912_njdstOH3MPzPKpadJUcucr7b-R-WLRQ5d-GqWIlGvnaggL_e_eFA54OJ8VN9NxcySu-qXjNzCWoUqosovYSXk9NO5OyBCs0baLCDfEdoyyiKEHgmgb_pYqgdjPzsA"}    />  
 
                                        <Carousel.Caption>  
-                                   <h3>Stay connected and Anonymous with HobbyT</h3>  
+                                   <h3 className="conn">Stay connected and Anonymous with HobbyT</h3>  
 
                                       </Carousel.Caption>  
 
