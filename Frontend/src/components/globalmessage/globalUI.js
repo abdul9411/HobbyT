5bfb6588add25153662e3e4c7562e6cc58d9199e
ayaan09 <79858880+ayaan09@users.noreply.github.com>
2021-04-29 22:32:10 +0600

    Update globalUI.js

--- Frontend/src/components/globalmessage/globalUI.js
+++ Frontend/src/components/globalmessage/globalUI.js
@@ -8,6 +8,12 @@ import TextAreaAutosize from 'react-textarea-autosize'
 function GlobalUI({user, token}) {
     
     const[messages, getmessages]= useState([])
+
+    /**
+   * get all message objects from the DB
+   * set the messages array to the fetched data
+   * if an error in encountered => display error
+   */
     useEffect(()=>{
         axios.defaults.headers.common['x-auth-token'] = token;
         axios.get(`http://localhost:3001/api/message/global`)
@@ -21,11 +27,20 @@ function GlobalUI({user, token}) {
 
     const [messagevalue, change]=useState("")
 
+    /**
+   * change the messagevalue to whatever the user is typing in the message textbox
+   * @param {Object} event
+   */
     function messagechange(event){
         change(event.target.value)
         console.log(messagevalue)
     }
 
+    /**
+   * triggered when user hits on the send button
+   * message object is sent to the DB
+   * if an error is encountered, it is displayed
+   */
     function postmessage(){
         axios.defaults.headers.common['x-auth-token'] = token;
         axios.post(`http://localhost:3001/api/message/global`, {
@@ -47,6 +62,7 @@ function GlobalUI({user, token}) {
         <div className="form">
         <TextAreaAutosize placeholder=" Aa" className="text-area-chat" value={messagevalue} onChange={messagechange}/>
          <button className="send-btn btn btn-sm btn-primary"onClick={postmessage}>Send</button></div>
+         {/* map all messages from messages array onto Messages template */}
         {messages.map(
             item=>(
               <Messages
