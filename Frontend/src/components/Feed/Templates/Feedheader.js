da4f442d30f46804d54f4c83062784f0e8d35754
ayaan09 <79858880+ayaan09@users.noreply.github.com>
2021-04-29 22:25:52 +0600

    Update Feedheader.js

--- Frontend/src/components/Feed/Templates/Feedheader.js
+++ Frontend/src/components/Feed/Templates/Feedheader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from "@material-ui/core"
 import './Feedheader.css'
-// const Community_name = 'Physics'
+
 
 function Feedheader(props) {
     return (
